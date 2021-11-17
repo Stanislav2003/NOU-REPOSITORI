924499f5b891bad09e8ce595945cfb67b9838a05
Stanislav2003 <94047550+Stanislav2003@users.noreply.github.com>
2021-11-17 11:38:03 +0100

    Add files via upload

---
+++ exercici20.java
@@ -0,0 +1,53 @@
+
+public class exercici20 {
+ 
+    public static void main(String[] args) {
+        int days=10;
+        String dia="english numbers";
+        for(int i = 0; i <= days; i++){
+            switch(i){
+            case 1:
+            dia="one";
+            break;
+            case 2:
+            dia="two";
+            break;
+            case 3:
+            dia="three";
+            break;
+            case 4:
+            dia="four";
+            break;
+            case 5:
+            dia="five";
+            break;
+            case 6:
+            dia="six";
+            break;
+            case 7:
+            dia="seven";
+            break;
+            case 8:
+            dia="eight";
+            break;
+            case 9:
+            dia="nine";
+            break;
+            case 10:
+            dia="ten";
+            break;
+            default:
+            dia="english numbers";
+            break;
+        }
+        System.out.println(dia);   
+        }
+           
+   
+
+
+
+
+}
+    }
+
